65139d42c8fad100f6f8ef2e306ced8c0fe9075c
Rohit <rohit.sahu@meesho.com>
2023-12-06 00:07:16 +0530

    update for dataengg

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 AdHoc Automations
 
+> Prerequisite: Python3.11
+>
+> gcloud application auth
+
 Get started
+
 ```
 git clone https://github.com/imRohitsahu/Adhoc-Automations.git
 cd Adhoc-Automations
@@ -15,20 +20,35 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 ```
+
 Step 2: Add all required variables in the .env file. For ex.
 
 ```
-PROJECT_ID='meesho-supply-prd-0622'
-EXCEL_FILE_PATH="jobs_info.xlsx"
-DEST_BUCKET_NAME="meesho-supply-v2"
-SOURCE_BUCKET_NAME="gcs-supl-fnf-pdf-prd"
+PROJECT_ID=meesho-supply-prd-0622
+EXCEL_FILE_PATH=jobs_info.xlsx          # excel sheet must have two column job_name and location
+DEST_BUCKET_NAME=meesho-supply-v2       # gcp bucket
+SOURCE_BUCKET_NAME=gcs-supl-fnf-pdf-prd    #aws bucket
+POOL_OPTION1=meesho-dataengg-sts-transfer-pool-1
+POOL_OPTION2=meesho-dataengg-sts-transfer-pool-2
+ENDPOINT1=bucket.vpce-0dc2fb490b50f02e2-uwoenjfk.s3.ap-southeast-1.vpce.amazonaws.com
+ENDPOINT2=bucket.vpce-0fb63f3ffd470c949-ntbhdiq9.s3.ap-southeast-1.vpce.amazonaws.com
+PREFIX="year=2023/,_delta_log/"           # Option if require to add prefix
+START_TIMEDELTA=10                     #Jobs will be schedule after X minutes of creation, this is to intertain the execution time delay
 ```
+
 Step 3: Create an Excel sheet with two columns `job_name` and `location` and save it as `jobs_info.xlsx`
 
+|     job_name     | location           |
+| :--------------: | ------------------ |
+| unique_job_name1 | path/to/directory1 |
+| unique_job_name2 | path/to/directory2 |
+
 Step 4: Just run the script.
+
 ```
 python3 scheduleStorageTransferJob_v3.py
 ```
+
 Step 5: Observe the  `transfer_job.log` and review the created jobs in the gcloud console.
 
 `Feel free to customize it as per requirement.`
--- scheduleStorageTransferJob_v3.py
+++ scheduleStorageTransferJob_v3.py
@@ -60,17 +60,19 @@ def checkPath(bucket_name, directory_path):
         return False
 
 if __name__== '__main__':
-    enableLogging()
-    project_id = os.environ.get("PROJECT_ID", "meesho-supply-prd-0622")
-    excel_file_path= os.environ.get("EXCEL_FILE_PATH", "jobs_info.xlsx")
-    dest_bucket_name=os.environ.get("DEST_BUCKET_NAME", "meesho-supply-v2")
-    source_bucket_name=os.environ.get("SOURCE_BUCKET_NAME", "gcs-supl-fnf-pdf-prd")
+    project_id = os.environ.get("PROJECT_ID", "")
+    excel_file_path= os.environ.get("EXCEL_FILE_PATH", "")
+    dest_bucket_name=os.environ.get("DEST_BUCKET_NAME", "")
+    source_bucket_name=os.environ.get("SOURCE_BUCKET_NAME", "")
+    pool_option1 = os.environ.get("POOL_OPTION1","")
+    pool_option2 = os.environ.get("POOL_OPTION2","")
+    endpoint1= os.environ.get("ENDPOINT1" ,"")
+    endpoint2= os.environ.get("ENDPOINT2" ,"")
+    start_timedelta= int(os.environ.get("START_TIMEDELTA" ,1))
+    prefix= os.environ.get("PREFIX" ,"" ).split(",")
+    
     jobs_df = pd.read_excel(excel_file_path, sheet_name='Sheet1', engine='openpyxl')
-    pool_option1 = os.environ.get("POOL_OPTION1","meesho-dataengg-sts-transfer-pool-1")
-    pool_option2 = os.environ.get("POOL_OPTION2","meesho-dataengg-sts-transfer-pool-2")
-    endpoint1= os.environ.get("ENDPOINT1" ,"bucket.vpce-0dc2fb490b50f02e2-uwoenjfk.s3.ap-southeast-1.vpce.amazonaws.com")
-    endpoint2= os.environ.get("ENDPOINT2" ,"bucket.vpce-0fb63f3ffd470c949-ntbhdiq9.s3.ap-southeast-1.vpce.amazonaws.com")
-
+    enableLogging()
     switchEndpoint=True
     switchPool = True
     transfer_client = storage_transfer_v1.StorageTransferServiceClient()
@@ -96,7 +98,7 @@ if __name__== '__main__':
         new_transfer_job.status = 1
 
         current_time = datetime.utcnow()
-        current_time= current_time+ timedelta(minutes=20)
+        current_time= current_time+ timedelta(minutes=start_timedelta)
         timestamp_pb = Timestamp()
         timestamp_pb.FromDatetime(current_time)
 
@@ -105,7 +107,7 @@ if __name__== '__main__':
         future_timestamp_pb.FromDatetime(future_time)
 
         start_seconds = timestamp_pb.seconds % 60
-        end_seconds = future_timestamp_pb.seconds % 60
+        # end_seconds = future_timestamp_pb.seconds % 60
 
         schedule_dict = {
             "schedule_start_date": {"year": current_time.year, "month": current_time.month, "day": current_time.day},
@@ -120,13 +122,10 @@ if __name__== '__main__':
             gcs_data_sink=storage_transfer_v1.GcsData(
                 bucket_name=dest_bucket_name,
                 path=f'{location}/'
-                # path=f'{location}'
             ),
             aws_s3_compatible_data_source=storage_transfer_v1.AwsS3CompatibleData(
                 bucket_name=source_bucket_name,
                 path=f'{location}/',
-                # path=f'{location}',
-                # endpoint="bucket.vpce-057a79cdc14bd9f9a-m1g7h019.s3.ap-southeast-1.vpce.amazonaws.com",
                 endpoint=f"{endpoint1 if switchEndpoint else endpoint2 }",
                 region='ap-southeast-1',
                 s3_metadata=storage_transfer_v1.S3CompatibleMetadata(
@@ -136,9 +135,7 @@ if __name__== '__main__':
                     list_api=storage_transfer_v1.S3CompatibleMetadata.ListApi.LIST_OBJECTS_V2
                 )
             ),
-            object_conditions=storage_transfer_v1.ObjectConditions(
-                include_prefixes=['year=2023/', '_delta_log/']
-            ),
+        
             transfer_options=storage_transfer_v1.TransferOptions(
                 overwrite_when=storage_transfer_v1.TransferOptions.OverwriteWhen.DIFFERENT,
                 metadata_options=storage_transfer_v1.MetadataOptions(
@@ -146,9 +143,13 @@ if __name__== '__main__':
                     time_created=storage_transfer_v1.MetadataOptions.TimeCreated.TIME_CREATED_PRESERVE_AS_CUSTOM_TIME
                 )
             ),
-            # source_agent_pool_name=f'projects/{project_id}/agentPools/meesho-supply-prd-sts-transfer-pool'
             source_agent_pool_name=f'projects/{project_id}/agentPools/{pool_option1 if switchPool else pool_option2}'
         )
+
+        if prefix[0]:
+            transfer_spec.object_conditions=storage_transfer_v1.ObjectConditions(
+                include_prefixes=prefix
+            )
         switchPool = not switchPool
         switchEndpoint = not switchEndpoint
         new_transfer_job.transfer_spec = transfer_spec
@@ -165,6 +166,5 @@ if __name__== '__main__':
             logging.error(f"An unexpected error occurred: {e}")
             logging.error(traceback.format_exc())
         # exit(0)
-        
 
     logging.info('Script completed.')
